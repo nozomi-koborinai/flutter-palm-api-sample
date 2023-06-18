8ab2c5ff1bb949f0099b557938bddeb4d5016530
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-06-18 14:25:22 +0900

    🚀 feat: add app widget

---
+++ lib/presentation/app.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:palm_sample/presentation/theme.dart';
+
+import '../application/state/overlay_loading_provider.dart';
+import 'components/loading.dart';
+import 'pages/home_page.dart';
+
+class App extends ConsumerWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      title: 'Summarize Text with PaLM API',
+      debugShowCheckedModeBanner: false,
+      theme: ref.read(themeProvider),
+      home: const HomePage(),
+      builder: (context, child) {
+        return Consumer(
+          builder: (context, ref, _) {
+            final isLoading = ref.watch(overlayLoadingProvider);
+            return Stack(
+              children: [
+                child!,
+                // ローディング表示
+                if (isLoading) const OverlayLoading()
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+}
