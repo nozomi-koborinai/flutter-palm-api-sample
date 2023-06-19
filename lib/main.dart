823b95b542d07d164f8f78c75260f43f2871a148
nozomi-koborinai <kobofender@gmail.com>
2023-06-19 21:41:57 +0900

    🔧 chore: add package firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:palm_sample/domain/summary/summary_repository.dart';
 import 'package:palm_sample/infrastructure/mock_summary_repository.dart';
 import 'package:palm_sample/presentation/app.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     ProviderScope(
       overrides: [
