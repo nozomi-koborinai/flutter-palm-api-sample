89b8a9da7bfe5478c71d2d09b414f5dca98f2d09
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-06-21 23:16:28 +0900

    ♻️ refactor: TextRepository.Impl. では追加後のドキュメントを監視して結果を待ってから値を返却するように修正

--- lib/infrastructure/firebase/text/text_repository_impl.dart
+++ lib/infrastructure/firebase/text/text_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:palm_sample/domain/text/entity/text.dart';
@@ -27,20 +29,40 @@ class TextRepositoryImpl implements TextRepository {
   @override
   Future<Text> summarizeText({required Text text}) async {
     try {
+      // 指定テキストを Firestore.text_documents コレクションへ追加する
       final doc = await collectionRef.add(
         TextDocument(status: {}, text: text.text, summary: ''),
       );
-      await Future.delayed(const Duration(seconds: 3));
-      final summaryText = await collectionRef.doc(doc.id).get();
+
+      // 指定したテキストが要約されるまで待つ
+      final summaryText = await fetchSummary(documentId: doc.id);
+
       return Text(
         id: doc.id,
-        text: summaryText.data()?.text ?? '',
-        summary: summaryText.data()?.summary ?? '',
+        text: summaryText?.data()?.text ?? '',
+        summary: summaryText?.data()?.summary ?? '',
       );
     } on FirebaseException catch (e) {
       throw ('Firestore の追加処理でエラーが発生しました: ${e.code}');
     } catch (e) {
       throw ('予期しないエラーが発生しました: $e');
     }
+  }
+
+  /// 指定テキストの要約結果を取得する
+  /// 指定したドキュメントの更新が終了するのを待ち続ける
+  Future<DocumentSnapshot<TextDocument>?> fetchSummary({
+    required String documentId,
+  }) async {
+    final snapshots = collectionRef.doc(documentId).snapshots();
+    const timeout = Duration(minutes: 1); // 監視のタイムアウトは 60 秒に設定する
+    await for (final snapshot in snapshots.timeout(timeout)) {
+      if (snapshot.data()?.summary != '') {
+        return snapshot;
+      }
+    }
+
+    // タイムアウトした場合は null を返却する
+    return null;
   }
 }
